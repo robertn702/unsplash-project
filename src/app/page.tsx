66ebaf3161d519105a34f1da8a297582b0746db3
Robert <robertn702@gmail.com>
2024-06-09 18:09:57 -0700

    Fetch unsplash photos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import ColorFilterSelect from "@/app/_components/color-filter-select";
 import SearchInput from "@/app/_components/search-input";
 import SortSelect from "@/app/_components/sort-select";
@@ -8,13 +10,45 @@ const unsplashKey = process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY;
 if (!unsplashKey) {
   throw new Error("unsplash access key is not defined");
 }
+import unsplash from "@/app/_libs/unsplash";
+import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "next/navigation";
+import { useMemo } from "react";
+import { ColorId, SearchOrderBy } from "unsplash-js";
 
-const unsplash = createApi({
-  accessKey: unsplashKey,
-});
-
+type UnsplashGetPhotosOptions = Parameters<typeof unsplash.search.getPhotos>[0];
 
 export default function Home() {
+  // todo: consider using params directly from props
+  const searchParams = useSearchParams();
+
+  const unsplashSearchOptions = useMemo<UnsplashGetPhotosOptions>(() => {
+    const query = searchParams.get("q");
+    const color = searchParams.get("color");
+    const orderBy = searchParams.get("order_by");
+    const page = searchParams.get("page");
+
+    // TODO: properly validate the search params
+    const options: UnsplashGetPhotosOptions = {
+      query: query || "",
+      color: color as ColorId | undefined,
+      orderBy: (orderBy || "latest") as SearchOrderBy,
+      page: page ? parseInt(page, 10) : 1
+    }
+
+    return options;
+  }, [searchParams])
+
+  const {data} = useQuery({
+    // TODO: Add relevant search params
+    queryKey: ["search", unsplashSearchOptions],
+    queryFn: () => {
+      return unsplash.search.getPhotos(unsplashSearchOptions)
+    },
+    // todo: enable when ready
+    enabled: false
+  })
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {/* search bar */}
@@ -25,6 +59,7 @@ export default function Home() {
         <SortSelect/>
       </div>
       {/* image grid */}
+
       {/* pagination */}
     </main>
   );
