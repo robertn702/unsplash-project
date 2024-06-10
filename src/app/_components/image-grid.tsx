289df8063b88ca7a4fcbcacb0c7558883709e10a
Robert <robertn702@gmail.com>
2024-06-09 19:33:55 -0700

    Update image grid styling

--- src/app/_components/image-grid.tsx
+++ src/app/_components/image-grid.tsx
@@ -9,15 +9,16 @@ export type ImageGridProps = Readonly<{
 
 export default function ImageGrid({images, isLoading}: ImageGridProps): ReactElement {
   if (isLoading) {
-    return <div>Loading...</div>
+    return <div className={`flex-grow`}>Loading...</div>
   }
 
   if (!isLoading && images.length === 0) {
-    return <div>{`We couldn't find any images that match your search`}</div>
+    return <div
+      className={`flex flex-grow items-center justify-center text-center`}>{`We couldn't find any images that match your search`}</div>
   }
 
   return (
-    <div className={"grid gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-4"}>
+    <div className={"flex-grow grid gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-4"}>
       {images.map((i) => (
         // TODO: calculate responsive heights
         <div className={"aspect-square w-full relative h-32"} key={i.id}>
